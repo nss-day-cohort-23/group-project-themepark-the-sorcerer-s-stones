ed7d3f89158958052df0dbe3757fe572725c803b
krieger27 <krieger@vols.utk.edu>
2018-01-11 15:01:03 -0600

    don't blow up

--- scripts/interactions.js
+++ scripts/interactions.js
@@ -10,3 +10,20 @@ $('.area').on("click", function() {
     let divId = this.id;
     controller.startAreaClick(divId);
 });
+    console.log('CLICK AREA', this);
+    controller.startAreaClick();
+});
+
+
+const timeNow = () => {
+    let currentTime = new Date(Date.now()),
+    currentHours = currentTime.getHours(),
+    currentMinutes = currentTime.getMinutes();
+    currentMinutes = currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes;
+
+    $("#time-selector").val(`${currentHours}:${currentMinutes}`);
+};
+
+// $('#time').bootstrapMaterialDatePicker({ date: false });  This isn't working!!! why??
+
+// $('time-selector').change(searchAttractionsByHour);
