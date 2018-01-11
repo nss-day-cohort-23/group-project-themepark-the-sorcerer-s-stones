e45115766bc2be956a1af19a26a6b7bf897cccd6
Eduardo DuQuesne <eduquesne@Eduardos-MacBook-Pro.local>
2018-01-11 13:49:21 -0600

    Areas to grid handlebars

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,3 +3,25 @@ let $ = require("jquery");
 let format = require("./format");
 let factory = require("./factory");
 let output = require("./output");
+
+
+module.exports.startAreas = () => {
+    factory.getAreas()
+    .then((parkAreas) => {
+        output.printToDom(parkAreas);
+    });
+};
+
+        
+module.exports.startAreaClick = () => {
+    let areas = factory.getAreas();
+    let attractions = factory.getAttr();
+    let attrTypes = factory.getAttrTypes();
+    Promise.all([areas, attractions, attrTypes])
+    .then((dataArray) => {
+        return format.formatAttr(dataArray[0], dataArray[1], dataArray[2]);        
+    })
+    .then((updateAttr) => {
+        // factory.openRides
+    });
+};
